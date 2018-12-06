a093d537227eac8542b2d14723c2319e216f9974
lshlsh135 <lshlsh135@naver.com>
2018-12-06 09:27:35 +0900

    김동영 위원의 correlation adjusted distance 계산하는 코드 시작

---
+++ 삼성증권김동영위원님/correlation_adjusted Distance.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 08:53:15 2018
+
+@author: 지형범
+"""
+
+
+
+import pandas as pd
+import numpy as np
+import cx_Oracle
+
+#이거 두개 반드시 선언!
+cx0=cx_Oracle.makedsn("localhost",1521,"xe")
+connection = cx_Oracle.connect("lshlsh135","2tkdgns2",cx0) #이게 실행이 안될때가 있는데
+kospi_day = pd.read_sql("""select * from kospi_day_prc_20181130""",con=connection) # 코스피 일별 종가
+kospi_day.set_index('TRD_DATE',inplace=True)
+datetime=pd.DataFrame(pd.to_datetime(kospi_day.index)) # 월말 날짜를 뽑기위해 datetime으로 바꿔줌
+kospi_day = pd.DataFrame(kospi_day, index = datetime['TRD_DATE'])
+
+
+
+base_data = kospi_day.iloc[len(kospi_day)-120:len(kospi_day),0]
