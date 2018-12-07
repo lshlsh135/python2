5066d5051f618158004b00c3088581e21ad14f89
lshlsh135 <lshlsh135@naver.com>
2018-12-07 17:03:56 +0900

    결과를 dictionary 형태로 저장

--- 삼성증권김동영위원님/correlation_adjusted Distance.py
+++ 삼성증권김동영위원님/correlation_adjusted Distance.py
@@ -24,24 +24,34 @@ datetime=pd.DataFrame(pd.to_datetime(kospi_day.index)) # 월말 날짜를 뽑기
 kospi_day = pd.DataFrame(kospi_day, index = datetime['TRD_DATE'])
 
 
+start_date = '2018-11-30'
 
-base_data = kospi_day.iloc[len(kospi_day)-120-60:len(kospi_day)-60,0]
+start_date_iloc = kospi_day.index.get_loc(kospi_day.index[kospi_day.index==start_date][0])
+end_date_iloc = start_date_iloc -120
+base_data = kospi_day.iloc[end_date_iloc+1:start_date_iloc+1,0]
 
-i=1
-compare_data = kospi_day.iloc[len(kospi_day)-1556-120:len(kospi_day)-1556,0]
-#두 지수를 비교하기 위해서는 시작을 100으로 만들어준다(index)
-base_data_index= base_data / base_data[-1] *100
-compare_data_index = compare_data / compare_data[-1] *100
+i=0
+range_start_date = '2005-12-29'
+result = dict()
+for i in range(len(kospi_day[kospi_day.index>=range_start_date])-241):
+        
+    compare_data = kospi_day.iloc[end_date_iloc+1-120-i:end_date_iloc+1-i,0]
+    #두 지수를 비교하기 위해서는 시작을 100으로 만들어준다(index)
+    base_data_index= base_data / base_data[-1] *100
+    compare_data_index = compare_data / compare_data[-1] *100
+    
+    diff = base_data_index.reset_index(drop=True) - compare_data_index.reset_index(drop=True)
+    diff_sq = diff**2
+    
+    #var_data = pd.concat([base_data_index.reset_index(drop=True),compare_data_index.reset_index(drop=True)])
+    #var_data = pd.concat([base_data.reset_index(drop=True),compare_data.reset_index(drop=True)])
+    var_data = kospi_day[(kospi_day.index>=range_start_date)&(kospi_day.index<=base_data.index[-1])]
+    ivar = (var_data.std() / var_data.mean() *100 ) **2
+    
+    indexed_distance = (diff_sq.sum() / ivar /120) ** (1/2)
+    correlation = stats.pearsonr(base_data,compare_data)[0]
+    
+    indexed_corr_adj_distance = indexed_distance + (1 - correlation)
 
-diff = base_data_index.reset_index(drop=True) - compare_data_index.reset_index(drop=True)
-diff_sq = diff**2
-
-#var_data = pd.concat([base_data_index.reset_index(drop=True),compare_data_index.reset_index(drop=True)])
-#var_data = pd.concat([base_data.reset_index(drop=True),compare_data.reset_index(drop=True)])
-var_data = kospi_day[(kospi_day.index>='2005-12-29')&(kospi_day.index<=base_data.index[-1])]
-ivar = (var_data.std() / var_data.mean() *100 ) **2
-
-indexed_distance = (diff_sq.sum() / ivar /120) ** (1/2)
-correlation = stats.pearsonr(base_data,compare_data)[0]
-
-indexed_corr_adj_distance = indexed_distance + (1 - correlation)
+    indexed_corr_adj_distance[0]
+    result[indexed_corr_adj_distance[0]]=compare_data_index
